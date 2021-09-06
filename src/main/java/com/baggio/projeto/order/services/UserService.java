e22a174423396897be5e8069546cd76c50f47263
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-09-06 14:09:06 -0300

    Camada de serviço, registro de componentes

---
+++ src/main/java/com/baggio/projeto/order/services/UserService.java
@@ -0,0 +1,28 @@
+package com.baggio.projeto.order.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.baggio.projeto.order.entities.User;
+import com.baggio.projeto.order.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> userOptional = userRepository.findById(id); 
+		return userOptional.get();
+	}
+	
+}
